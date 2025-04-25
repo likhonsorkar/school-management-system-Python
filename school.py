a78c71c748fb2207967f60b5c8729eb7e7733d5b
unknown <likhonsorkar002@gmail.com>
2025-04-25 11:45:20 +0600

    school class some work

--- school.py
+++ school.py
@@ -5,14 +5,41 @@ class School:
         self.teachers = {}
         self.classrooms = {}
     def add_classroom(self,classroom):
-        pass
+        self.classrooms[classroom.name] = classroom
     def add_teacher(self, subject, teacher):
-        pass
+        self.teachers[subject] = teacher
     def student_admission(self, student):
         pass
-    def calculate_grade(self, marks):
+    @staticmethod
+    def calculate_grade(marks):
+        if 80<= marks >=100:
+            return 'A+'
+        elif 70<= marks >=79:
+            return 'A'
+        elif 60<= marks >=69:
+            return 'A-'
+        elif 50<= marks >=59:
+            return 'B'
+        elif 40<= marks >=49:
+            return 'C'
+        elif 33<= marks >=39:
+            return 'D'
+        else:
+            return 'F'
+    @staticmethod
+    def grade_to_value(grade):
+        grade_map = {
+            'A+' : 5.00,
+            'A'  : 4.00,
+            'A-' : 3.5,
+            'B'  : 3.0,
+            'C'  : 2.0,
+            'D'  : 1.0,
+            'F'  : 0.00
+        }
+        return grade_map[grade]
+    @staticmethod
+    def value_to_grade(value):
         pass
-    def grade_to_value(self, grade):
-        pass
-    def value_to_grade(self, value):
+    def __repr__(self):
         pass
