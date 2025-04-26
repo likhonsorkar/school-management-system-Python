d9c83f43bd4ea1ec39577844550b7835c34fb3b8
unknown <likhonsorkar002@gmail.com>
2025-04-26 10:22:27 +0600

    Complete

--- classroom.py
+++ classroom.py
@@ -1,13 +1,17 @@
 class ClassRoom:
-    def __init__(self, name, students, subjects):
+    def __init__(self, name) -> None:
         self.name = name
-        self.students = students
-        self.subjects = subjects
-    def add_student(self,student):
-        pass
-    def add_subject(self,subject):
-        pass
-    def take_semester_final(self):
-        pass
-    def get_top_students(self):
-        pass
+        self.students = [] # list of student objects
+        self.subjects = [] # list of subject objects
+    def add_student(self, student): # rahim, eight e vorti hobe. 
+        roll_no = f"{self.name}-{len(self.students)+1}" # eight-2
+        student.id = roll_no
+        self.students.append(student)
+    def add_subject(self, subject):
+        self.subjects.append(subject)
+    def take_semester_final_exam(self):
+        for subject in self.subjects:
+            subject.exam(self.students)
+        for student in self.students:
+            student.calculate_final_grade()
+        
